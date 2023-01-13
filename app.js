a0f4fb8bc2518c5af51b20c61a378a00fa30134a
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-01-13 21:09:24 +0300

    Update_13_01_2023

--- app.js
+++ app.js
@@ -3,15 +3,3 @@ const headline = document.getElementById('headline');
 const items = document.querySelectorAll('li');
 const highlights = document.querySelectorAll('.highlight');
 
-for ( const highlight of highlights ) {
-  highlight.style.backgroundColor = 'cornsilk';
-}
-
-for (let i = 0; i < items.length; i++) {
-  items[i].style.color = 'orchid';
-}
-
-btnUpdate.addEventListener('click', () => {
-  headline.style.border = 'solid 2px red';
-  headline.style.fontSize = '60px';													 
-});
